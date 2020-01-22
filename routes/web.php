61977b3ddf37de640b1db7d84344d417ec1f87e9
Alfi Yudin <alfiyudin46@gmail.com>
2020-01-22 11:48:11 +0700

    optional param & migrate

--- routes/web.php
+++ routes/web.php
@@ -57,3 +57,76 @@ Route::get('about/biodata/{nama}/{alamat}/{sekolah}/{umur}', function($a,$b,$c,$
             '<br>Sekolah di ' . $c . 
             ' dan Ber-umur ' . $d . ' Tahun';
 });
+
+Route::get('name/{param?}', function($a = "Alfi"){
+    return 'Your Name is ' . $a;
+}); 
+
+Route::get('pesan/{makan?}/{minum?}/{harga?}', function($a = null, $b = null, $c = null){
+    if(!$a && !$b){
+        return 'Silahkan Pesan Dahulu';
+    }
+    if(isset($a)){
+        echo 'Anda Pesan ' . $a;
+    }
+    if(isset($b)){
+        echo ' dan ' . $b;
+    }
+    if(isset($c)){
+        if($c >= 35000){
+            $size = "Large";
+        }
+        elseif($c >= 25000){
+            $size = "Medium";
+        }
+        elseif($c >= 0){
+            $size = "Small";
+        }
+        else{
+            return " Harga Tidak Valid";
+        }
+        echo ' dan mendapatkan size ' . $size.' dengan harga '.$c;
+    }
+}); 
+
+Route::get('tni/{nama?}/{bb?}/{umur?}', function($a = null, $b = null, $c = null){
+    if(!$a && !$b){
+        return 'Silahkan Isi Data Terlebih Dahulu';
+    }
+    if(isset($a)){
+        echo 'Nama Anda ' . $a;
+    }
+    if(isset($b)){
+        if($b >= 76 && $b <=100){
+            $bb = "Anda Harus turun berat badan!";
+        }
+        elseif($b >= 65 && $b < 76){
+            $bb = "Berat badan anda ideal!";
+        }
+        elseif($b >= 50 && $b < 65){
+            $bb = "Naikan berat badan anda!";
+        }
+        elseif($b < 50){
+            $bb = "Anda Kurang Nutrisi";
+        }
+        else{
+            $bb = "Berat badan tidak valid";
+        }
+        echo ' '. $bb;
+    }
+    if(isset($c)){
+        if($c >= 50 && $c <=60){
+            $pangkat = "Jendral";
+        }
+        elseif($c >= 40 && $c < 50){
+            $pangkat = "Laksamana";
+        }
+        elseif($c >= 30 && $c < 40){
+            $pangkat = "Perwira";
+        }
+        else{
+            $pangkat = "umur tidak valid";
+        }
+        echo ' ' .$pangkat;
+    }
+}); 
